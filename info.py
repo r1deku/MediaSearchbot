ce7c776005de304340eb1f3bd784886e7690cddc
BitterBeast <141508197+BitterBeast@users.noreply.github.com>
2023-11-30 19:14:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -6,9 +6,9 @@ id_pattern = re.compile(r'^.\d+$')
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ.get('API_ID', '28376650'))
+API_HASH = environ.get('API_HASH', '6200fc0feb5ff2678bf904194c8696fb')
+BOT_TOKEN = environ.get('BOT_TOKEN', '6104978506:AAGkw-5XOgSOJOZ1pHCyOPy1iw2bauq02MA')
 USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
 
 # Bot settings
@@ -24,16 +24,19 @@ auth_channel = environ.get('AUTH_CHANNEL')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
+DATABASE_URI = environ.get('DATABASE_URI', 'mongodb+srv://pkr923366:pkr923366@cluster0.nc5ygqe.mongodb.net/?retryWrites=true&w=majority')
+DATABASE_NAME = environ.get('DATABASE_NAME', 'Cluster0')
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Messages
 default_start_msg = """
-**Hi, I'm Media Search bot**
+Hi, I'm Media Search bot
 
-Here you can search files in inline mode. Just press following buttons and start searching.
-"""
+Here you can search files in inline mode. Just press following buttons SEARCH HERE and start searching or Just type @InlineFilmBot in any chat and type the name of the movie with a space
+
+Eg: @InlineFilmBot MOVIE NAME
+
+NB: To use this bot first you need to subscribe @InlineFilmUpdate"""
 
 START_MSG = environ.get('START_MSG', default_start_msg)
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
